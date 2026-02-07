84bc8b1353a417eccbeeaa9d5a1d3a035c969446
Eric Hausman <109166965+hausmaneric@users.noreply.github.com>
2026-02-07 18:12:57 -0300

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('nosso_mini');
+  currentYear: number = new Date().getFullYear();
+
+  scrollToSection(id: string) {
+    const element = document.getElementById(id);
+    element?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  }
 }
